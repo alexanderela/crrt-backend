52f25e66b833bfc3d9c9fc663c27bedd588be650
Alexander Ela <ela.alexander@gmail.com>
2019-07-16 16:19:41 -0600

    Add passing tests for GET /api/v1/cases

--- test/server.test.js
+++ test/server.test.js
@@ -25,27 +25,55 @@ describe('server', () => {
 	});
 
 	describe('GET /api/v1/cases', () => {
-		it('should return 200 status code on successful request', done => {});
-		it('should return 404 status code for route that does not exist', done => {});
-		it('should return 500 status code on unsuccessful request', done => {});
-		it('should return an array of cases', done => {});
+		it('should return 200 status code on successful request', done => {
+			chai.request(app)
+				.get('/api/v1/cases')
+				.end((error, response) => {
+					expect(response).to.have.status(200);
+					done();
+				});
+		});
+		
+		it('should return 404 status code for route that does not exist', done => {
+			chai.request(app)
+				.get('/api/v1/casez')
+				.end((error, response) => {
+					expect(response).to.have.status(404);
+					done();
+				})
+		});
+
+		it('should return an array of cases', done => {
+			chai.request(app)
+				.get('/api/v1/cases')
+				.end((error, response) => {
+					const pastMedicalHistory1 = JSON.parse(response.body[0].historyOfPresentIllness).pastMedicalHistory
+					const pastMedicalHistory2 = JSON.parse(response.body[1].historyOfPresentIllness).pastMedicalHistory
+
+					expect(response.body[0].id).to.equal(1)
+					expect(pastMedicalHistory1[2]).to.equal('COPD')
+					expect(response.body[1].id).to.equal(2)
+					expect(pastMedicalHistory2[2]).to.equal('Chronic pancreatitis')
+					done();
+				})
+		});
 	});
 
 	describe('POST /api/v1/cases', () => {
-		it('should return 201 status code on successful request', done => {});
-		it('should return 415 status code for improper formatting', done => {});
-		it('should return 404 status code when id not found', done => {});
-		it('should return 500 status code on unsuccessful request', done => {});
+		it.skip('should return 201 status code on successful request', done => {});
+		
+		it.skip('should return 415 status code for improper formatting', done => {});
+		
+		it.skip('should return 404 status code when id not found', done => {});
 	});
 	
 	describe('PATCH /api/v1/cases/:id', () => {
-		it('should return 204 status code on successful request', done => {});
-		it('should return 415 status code for improper formatting', done => {});
-		it('should return 500 status code on unsuccessful request', done => {});
+		it.skip('should return 204 status code on successful request', done => {});
+		
+		it.skip('should return 415 status code for improper formatting', done => {});
 	});
 	
 	describe('DELETE /api/v1/cases/:id', () => {
-		it('should return 200 status code on successful request', done => {});
-		it('should return 500 status code on unsuccessful request', done => {});
+		it.skip('should return 200 status code on successful request', done => {});
 	});
 });
