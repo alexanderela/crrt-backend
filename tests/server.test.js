39950be20e79026a4e886e3994cc3a02ea2b830f
Alexander Ela <ela.alexander@gmail.com>
2019-07-15 19:36:16 -0600

    Add before and after blocks to main server test describe block

--- tests/server.test.js
+++ tests/server.test.js
@@ -8,7 +8,22 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
 
+chai.use(chaiHttp)
+
 describe('server', () => {
+	before(done => {
+		database.migrate.rollback()
+			.then(() => database.migrate.latest())
+			.then(() => database.seed.run())
+			.then(() => done())
+	});
+
+	after(done => {
+		database.migrate.rollback()
+			.then(() => console.log('Testing complete.  DB rolled back'))
+			.then(() => done())
+	});
+
 	describe('GET /api/v1/cases', () => {});
 
 	describe('POST /api/v1/cases', () => {});
