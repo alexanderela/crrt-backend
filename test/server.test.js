006673068bfb03b73f7707f333cdd8b26e0929e8
Alexander Ela <ela.alexander@gmail.com>
2019-07-16 00:01:42 -0600

    Correct filepath for configuration declaration in server.test.js

--- test/server.test.js
+++ test/server.test.js
@@ -5,7 +5,7 @@ const chaiHttp = require('chai-http');
 const expect = chai.expect;
 const app = require('../server');
 const environment = process.env.NODE_ENV || 'development'
-const configuration = require('./knexfile')[environment]
+const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
 chai.use(chaiHttp)
