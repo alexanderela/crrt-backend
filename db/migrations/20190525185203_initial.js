d85fb97d3c992314700455170c4e46b337dcf7a9
Alexander Ela <ela.alexander@gmail.com>
2019-10-02 19:54:36 -0600

    Add warningRanges to cases and cases-test databases.

--- db/migrations/20190525185203_initial.js
+++ db/migrations/20190525185203_initial.js
@@ -1,30 +1,28 @@
-
 exports.up = function(knex, Promise) {
   return Promise.all([
-  	knex.schema.createTable('cases', function(table) {
-  		table.increments('id').primary();
-  		table.decimal('sodiumProductionRate');
-  		table.decimal('potassiumProductionRate');
-  		table.decimal('chlorideProductionRate');
-  		table.decimal('bicarbonateProductionRate');
-  		table.decimal('BUNProductionRate');
-  		table.decimal('creatinineProductionRate');
-  		table.decimal('calciumProductionRate');
-  		table.decimal('filtrationFractionStarting');
-  		table.text('gender');
-  		table.decimal('usualWeight');
-  		table.text('historyOfPresentIllness');
-  		table.text('vitalSigns');
-  		table.text('medications');
-  		table.text('imaging');
-  		table.text('physicalExam');
-  		table.timestamps(true, true);
-  	})
-  ])
+    knex.schema.createTable("cases", function(table) {
+      table.increments("id").primary();
+      table.decimal("sodiumProductionRate");
+      table.decimal("potassiumProductionRate");
+      table.decimal("chlorideProductionRate");
+      table.decimal("bicarbonateProductionRate");
+      table.decimal("BUNProductionRate");
+      table.decimal("creatinineProductionRate");
+      table.decimal("calciumProductionRate");
+      table.decimal("filtrationFractionStarting");
+      table.text("gender");
+      table.decimal("usualWeight");
+      table.text("historyOfPresentIllness");
+      table.text("vitalSigns");
+      table.text("medications");
+      table.text("imaging");
+      table.text("physicalExam");
+      table.text("warningRanges");
+      table.timestamps(true, true);
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.all([
-  	knex.schema.dropTable('cases')
-  ]);
+  return Promise.all([knex.schema.dropTable("cases")]);
 };
