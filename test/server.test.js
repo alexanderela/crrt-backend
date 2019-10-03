fe398bb0f3e89ea387e0a4fce87384ae1bd47dab
Alexander Ela <ela.alexander@gmail.com>
2019-10-03 17:36:22 -0600

    Remove console.log

--- test/server.test.js
+++ test/server.test.js
@@ -70,7 +70,6 @@ describe("server", () => {
           expect(response.body.length).to.not.equal(0);
           expect(response.body[0].id).to.equal(1);
           expect(pastMedicalHistory1[2]).to.equal("COPD");
-          console.log(response.body);
           expect(response.body[1].id).to.equal(2);
           expect(pastMedicalHistory2[2]).to.equal("Chronic pancreatitis");
           done();
