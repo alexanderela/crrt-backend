efb34bfeabc00654b77cc4060a1da727a39f755a
Alexander Ela <ela.alexander@gmail.com>
2019-10-02 20:22:41 -0600

    Fix failing PUT test after adding warningRanges.

--- db/seeds/test/cases_test.js
+++ db/seeds/test/cases_test.js
@@ -1,34 +1,41 @@
-const patientCases = require('../../../caseSeedData.js');
+const patientCases = require("../../../caseSeedData.js");
 
 const createCase = (knex, patientCase) => {
-  return knex('cases').insert({
-    sodiumProductionRate: patientCase.sodiumProductionRate,
-    potassiumProductionRate: patientCase.potassiumProductionRate,
-    chlorideProductionRate: patientCase.chlorideProductionRate,
-    bicarbonateProductionRate: patientCase.bicarbonateProductionRate,
-    BUNProductionRate: patientCase.BUNProductionRate,
-    creatinineProductionRate: patientCase.creatinineProductionRate,
-    calciumProductionRate: patientCase.calciumProductionRate,
-    filtrationFractionStarting: patientCase.filtrationFractionStarting,
-    gender: patientCase.gender,
-    usualWeight: patientCase.usualWeight,
-    historyOfPresentIllness: JSON.stringify(patientCase.historyOfPresentIllness),
-    vitalSigns: JSON.stringify(patientCase.vitalSigns),
-    medications: JSON.stringify(patientCase.medications),
-    imaging: JSON.stringify(patientCase.imaging),
-    physicalExam: JSON.stringify(patientCase.physicalExam)
-  }, 'id');
-}
+  return knex("cases").insert(
+    {
+      sodiumProductionRate: patientCase.sodiumProductionRate,
+      potassiumProductionRate: patientCase.potassiumProductionRate,
+      chlorideProductionRate: patientCase.chlorideProductionRate,
+      bicarbonateProductionRate: patientCase.bicarbonateProductionRate,
+      BUNProductionRate: patientCase.BUNProductionRate,
+      creatinineProductionRate: patientCase.creatinineProductionRate,
+      calciumProductionRate: patientCase.calciumProductionRate,
+      filtrationFractionStarting: patientCase.filtrationFractionStarting,
+      gender: patientCase.gender,
+      usualWeight: patientCase.usualWeight,
+      historyOfPresentIllness: JSON.stringify(
+        patientCase.historyOfPresentIllness
+      ),
+      vitalSigns: JSON.stringify(patientCase.vitalSigns),
+      medications: JSON.stringify(patientCase.medications),
+      imaging: JSON.stringify(patientCase.imaging),
+      physicalExam: JSON.stringify(patientCase.physicalExam),
+      warningRanges: JSON.stringify(patientCase.warningRanges)
+    },
+    "id"
+  );
+};
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('cases').del()
+  return knex("cases")
+    .del()
     .then(() => {
       let casePromises = [];
-        patientCases.forEach(patientCase => {
-          casePromises.push(createCase(knex, patientCase));
-        });
-        return Promise.all(casePromises);
+      patientCases.forEach(patientCase => {
+        casePromises.push(createCase(knex, patientCase));
+      });
+      return Promise.all(casePromises);
     })
-    .catch(error => console.log(`Error seeding testing DB data: ${error}`))
+    .catch(error => console.log(`Error seeding testing DB data: ${error}`));
 };
--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const environment = process.env.NODE_ENV || 'development';
-const config = require('./knexfile')[environment];
-const database = require('knex')(config);
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const environment = process.env.NODE_ENV || "development";
+const config = require("./knexfile")[environment];
+const database = require("knex")(config);
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
-app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + "/public"));
+app.set("port", process.env.PORT || 3000);
 
 //Endpoints
-app.get('/api/v1/cases', (request, response) => {
-	database('cases').select()
-	.then(cases => response.status(200).json(cases))
-	.catch(error => response.status(500).json({
-		error: error.message
-	}));
+app.get("/api/v1/cases", (request, response) => {
+  database("cases")
+    .select()
+    .then(cases => response.status(200).json(cases))
+    .catch(error =>
+      response.status(500).json({
+        error: error.message
+      })
+    );
 });
 
-app.post('/api/v1/cases', (request, response) => {
-	const patientCase = request.body;
+app.post("/api/v1/cases", (request, response) => {
+  const patientCase = request.body;
 
-	for(let requiredParam of [
-		"sodiumProductionRate",
+  for (let requiredParam of [
+    "sodiumProductionRate",
     "potassiumProductionRate",
     "chlorideProductionRate",
     "bicarbonateProductionRate",
@@ -38,31 +41,37 @@ app.post('/api/v1/cases', (request, response) => {
     "vitalSigns",
     "medications",
     "imaging",
-    "physicalExam"
-	]) {
-		if(patientCase[requiredParam] === undefined) {
-		 return response.status(415).json({
+    "physicalExam",
+    "warningRanges"
+  ]) {
+    if (patientCase[requiredParam] === undefined) {
+      return response.status(415).json({
         error: `You're missing the ${requiredParam} property.`
       });
-		}
-	}
+    }
+  }
 
-	database('cases').insert(patientCase, 'id')
-		.then(caseIds => response.status(201).json({
-			id: caseIds[0],
-			message: `Case successfully created!`
-		}))
-		.catch(error => response.status(500).json({
-			error: error.message
-		}));
+  database("cases")
+    .insert(patientCase, "id")
+    .then(caseIds =>
+      response.status(201).json({
+        id: caseIds[0],
+        message: `Case successfully created!`
+      })
+    )
+    .catch(error =>
+      response.status(500).json({
+        error: error.message
+      })
+    );
 });
 
-app.put('/api/v1/cases/:id', (request, response) => {
-	const { id } = request.params;
-	const newEntry = request.body;
+app.put("/api/v1/cases/:id", (request, response) => {
+  const { id } = request.params;
+  const newEntry = request.body;
 
-	for(let requiredParam of [
-		"sodiumProductionRate",
+  for (let requiredParam of [
+    "sodiumProductionRate",
     "potassiumProductionRate",
     "chlorideProductionRate",
     "bicarbonateProductionRate",
@@ -76,46 +85,59 @@ app.put('/api/v1/cases/:id', (request, response) => {
     "vitalSigns",
     "medications",
     "imaging",
-    "physicalExam"
-	]) {
-		if(newEntry[requiredParam] === undefined || newEntry[requiredParam] === null) {
-		 return response.status(415).json({
+    "physicalExam",
+    "warningRanges"
+  ]) {
+    if (
+      newEntry[requiredParam] === undefined ||
+      newEntry[requiredParam] === null
+    ) {
+      return response.status(415).json({
         error: `You're missing the ${requiredParam} property.`
       });
-		}
-	}
+    }
+  }
 
-	database('cases').where('id', id)
-		.update(newEntry)
-		.then(caseId => {
-			response.status(202).json({
-				id: caseId,
-				message: `Edit successful. Case with id of ${id} has been updated.`
-			});
-		})
-		.catch(error => response.status(500).json({
-			error: error.message
-		}));
+  database("cases")
+    .where("id", id)
+    .update(newEntry)
+    .then(caseId => {
+      response.status(202).json({
+        id: caseId,
+        message: `Edit successful. Case with id of ${id} has been updated.`
+      });
+    })
+    .catch(error =>
+      response.status(500).json({
+        error: error.message
+      })
+    );
 });
 
-app.delete('/api/v1/cases/:id', (request, response) => {
-	const { id } = request.params;
-	database('cases').where('id', id).delete()
-		.then(caseId => response.status(200).json({
-			id: id,
-			message: `Case ${id} has been deleted.`
-		}))
-		.catch(error => response.status(500).json({
-			error: `Error deleting case: ${error.message}`
-		}));
+app.delete("/api/v1/cases/:id", (request, response) => {
+  const { id } = request.params;
+  database("cases")
+    .where("id", id)
+    .delete()
+    .then(caseId =>
+      response.status(200).json({
+        id: id,
+        message: `Case ${id} has been deleted.`
+      })
+    )
+    .catch(error =>
+      response.status(500).json({
+        error: `Error deleting case: ${error.message}`
+      })
+    );
 });
 
 app.use((request, response) => {
-	response.status(404).sendFile(__dirname + '/public/404.html');
+  response.status(404).sendFile(__dirname + "/public/404.html");
 });
 
-app.listen(app.get('port'), () => {
-	console.log(`Server is running on ${app.get('port')}.`)
+app.listen(app.get("port"), () => {
+  console.log(`Server is running on ${app.get("port")}.`);
 });
 
 module.exports = app;
