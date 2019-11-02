d368657c09c547ac62d1413d113ec31099c44ac1
Alexander Rau <alexanderrau86@yahoo.com>
2019-11-02 15:04:45 -0600

    Fix data formatting to help FE

--- caseTestData.js
+++ caseTestData.js
@@ -12,25 +12,22 @@ const caseStudies = [
     usualWeight: 86.8,
     historyOfPresentIllness: {
       overview: [
-        "A 72 year old lady with a history of HTN, COPD, and DM is brought to the Emergency Department by ambulance after being found unresponsive by family members.",
-        "Upon arrival she is found to be in acute hypoxemic respiratory failure requiring emergent intubation.",
-        "Initial vital signs are temperature 39.1 C, HR 128, BP 78/53, RR 30.",
-        "A chest x-ray shows a left lower lobe infiltrate.",
-        "The patient is started on norepinephrine.",
-        "Creatinine is 2.7, up from 0.86 one month earlier.",
-        "Her urine output in the first 4 hours is 44 cc’s of urine.",
+        "A 72 year old lady with a history of HTN, COPD, and DM is brought to the Emergency Department by ambulance after being found unresponsive by family members.  ",
+        "Upon arrival she is found to be in acute hypoxemic respiratory failure requiring emergent intubation.  ",
+        "Initial vital signs are temperature 39.1 C, HR 128, BP 78/53, RR 30.  ",
+        "A chest x-ray shows a left lower lobe infiltrate.  ",
+        "The patient is started on norepinephrine.  ",
+        "Creatinine is 2.7, up from 0.86 one month earlier.  ",
+        "Her urine output in the first 4 hours is 44 cc’s of urine.  ",
         "The decision is made to start the patient on continuous renal replacement therapy. A 15 cm Mahurkar in the right internal jugular vein."
       ],
       pastMedicalHistory: [
-        "Hypertension",
-        "Insulin-dependent Diabetes Mellitus Type 2",
-        "COPD",
-        "CKD 3 (baseline creatinine 1.4 - 1.6)"
+        "Hypertension, Insulin-dependent Diabetes Mellitus Type 2, COPD, CKD 3 (baseline creatinine 1.4 - 1.6)"
       ],
       pastSurgicalHistory: ["Abdominal hernia repair"],
       socialHistory: [
-        "Current smoker, 1/2 pack per day. Has been smoking since age 17.",
-        "No alcohol or other drug use.",
+        "Current smoker, 1/2 pack per day. Has been smoking since age 17.  ",
+        "No alcohol or other drug use.  ",
         "Retired from work in retail."
       ],
       familyHistory: ["No family history of renal disease."]
@@ -204,25 +201,20 @@ const caseStudies = [
     usualWeight: 86.8,
     historyOfPresentIllness: {
       overview: [
-        "Jackie Smith is a 51 year old lady with a history of alcohol abuse, chronic pancreatitis, major depressive disorder, and recurrent c. diff colitis who was brought to the Emergency Department by ambulance after being found unresponsive by family members.",
-        "Empty bottles of Percocet and Tylenol were found nearby.  Initial labs reveal an AST of 12,000 and an INR of 4.1.",
-        "Upon arrival she is unresponsive and not protecting her airway.  She undergoes emergent intubation.",
-        "Initial vital signs are HR 130, BP 83/37, RR 28.  She is started on norepinephrine and vasopressin.",
-        "She does not make any urine while in the emergency department, and intial creatinine is 3.1, up from 0.46 one month earlier.",
+        "Jackie Smith is a 51 year old lady with a history of alcohol abuse, chronic pancreatitis, major depressive disorder, and recurrent c. diff colitis who was brought to the Emergency Department by ambulance after being found unresponsive by family members.  ",
+        "Empty bottles of Percocet and Tylenol were found nearby.  Initial labs reveal an AST of 12,000 and an INR of 4.1.  ",
+        "Upon arrival she is unresponsive and not protecting her airway.  She undergoes emergent intubation.  ",
+        "Initial vital signs are HR 130, BP 83/37, RR 28.  She is started on norepinephrine and vasopressin.  ",
+        "She does not make any urine while in the emergency department, and intial creatinine is 3.1, up from 0.46 one month earlier.  ",
         "The decision is made to start the patient on continuous renal replacement therapy.  A 15 cm Mahurkar catheter is placed in the right internal jugular vein."
       ],
       pastMedicalHistory: [
-        "Hypertension",
-        "Alcohol abuse",
-        "Chronic pancreatitis",
-        "Pulmonary embolism",
-        "Diffuse chronic pain",
-        "Major Depressive Disorder"
+        "Hypertension, Alcohol abuse, Chronic pancreatitis, Pulmonary embolism, Diffuse chronic pain, Major Depressive Disorder"
       ],
       pastSurgicalHistory: ["Back surgery", "Hysterectomy"],
       socialHistory: ["Never smoker", "Heavy alcohol use, ongoing"],
       familyHistory: [
-        "No family history of renal disease on mom's side.",
+        "No family history of renal disease on mom's side.  ",
         "Dad's history is unknown"
       ]
     },
